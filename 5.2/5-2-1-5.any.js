1e058e74e94894efbad5d07984c40e1054012422
Seungwon Go <seungwon.go@gmail.com>
2023-05-19 07:32:36 +0900

    Update 5-2-1-5.any.js

--- 5.2/5-2-1-5.any.js
+++ 5.2/5-2-1-5.any.js
@@ -1,11 +1,11 @@
 const promise1 = Promise.reject(0);
 const promise2 = new Promise((resolve, reject) => {
-  // 500ms 후에 resolve('one') 함수 호출
+  // 500ms 후에 resolve 함수를 실행합니다.
   setTimeout(resolve, 500, "500ms 후에 실행됩니다.");
 });
 
 const promise3 = new Promise((resolve, reject) => {
-  // 100ms 후에 resolve('two') 함수 호출
+  // 100ms 후에 resolve 함수를 실행합니다.
   setTimeout(resolve, 100, "100ms 후에 실행됩니다.");
 });
 
